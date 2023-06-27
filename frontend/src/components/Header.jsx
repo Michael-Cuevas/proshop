1d76a2cbbb62e1c7610b0bf71739df2c3153170a
Michael-Cuevas <mikecuevas1229@gmail.com>
2023-06-26 21:31:28 -0700

    added prod details page and began set up backend

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import {Navbar, Nav, Container, NavLink} from 'react-bootstrap';
 import {FaShoppingCart, FaUser} from 'react-icons/fa';
+import { LinkContainer } from 'react-router-bootstrap'
 import logo from '../assets/logo.png'
 
 import React from 'react'
@@ -9,19 +10,25 @@ const Header = () => {
     <header>
         <Navbar bg = "dark" variant="dark" expand="lg" collapseOnSelect>
             <Container>
-                <Navbar.Brand href="/">
-                    <img src={logo} alt='ProShop' />
-                    ProShop
-                </Navbar.Brand>
+                <LinkContainer to='/'>
+                    <Navbar.Brand>
+                        <img src={logo} alt='ProShop' />
+                        ProShop
+                    </Navbar.Brand>
+                </LinkContainer>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto">
-                        <Nav.Link href="/cart">
-                            <FaShoppingCart/> Cart
-                        </Nav.Link>
-                        <NavLink href="/login">
-                            <FaUser/> Sign In                           
-                        </NavLink>
+                        <LinkContainer to='/cart'>
+                            <Nav.Link>
+                                <FaShoppingCart/> Cart
+                            </Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to='/login'>
+                            <NavLink>
+                                <FaUser/> Sign In                           
+                            </NavLink>
+                        </LinkContainer>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
